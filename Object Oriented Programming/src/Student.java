4dbea8fd640ab0bb894ef25767b3d7e511d83d23
vempatisiva <vempatisivanarasimharao666@gmail.com>
2024-01-11 10:43:00 -0600

    Adding OOP Project

---
+++ Object Oriented Programming/src/Student.java
@@ -0,0 +1,19 @@
+public class Student {
+
+	String name;
+	public String getName()
+	{
+		name="siva Narasimharao Vempati";
+		//System.out.println(name);
+		return name;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student S1 = new Student();
+				
+		System.out.println(S1.getName());
+	}
+
+	
+}
