01e3daa8a823f473da90bbbcca48f7c96c53d114
vempatisiva <vempatisivanarasimharao666@gmail.com>
2024-01-12 09:36:38 -0600

    Adding Overloading project

---
+++ Overloading/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(2,3));
+		System.out.println(add(2,3,4));
+	}
+	
+	public static int add(int a , int b) {
+		return a+b;
+	}
+	
+	public static int add(int a , int b , int c) {
+		return a+b+c;
+	}
+}
