e21ad23a5fce4dbc8a36f18dbed08dd4239db70f
vempatisiva <vempatisivanarasimharao666@gmail.com>
2024-01-22 09:16:42 -0600

    ADDING OOPS PROJECT

---
+++ CoreJavaDevelopment/src/oops/Student.java
@@ -0,0 +1,21 @@
+package oops;
+
+public class Student {
+
+	String name;
+	public String getName()
+	{
+		name="siva";
+		//System.out.println(name);
+		return name;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student S1 = new Student();
+				
+		System.out.println(S1.getName());
+	}
+
+	
+}
